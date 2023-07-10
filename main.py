89f38148c3e24aa23bfa5d2a062d15c5342a2698
leoworks2020 <leoworks2020@gmail.com>
2023-07-10 15:23:23 -0300

    Updates in main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@
 # Objectives: Enhance skill on Python by Solving Challenges
 #
 # Project Structure:
-# Each .py file has an individual challenge, where DocString will present the challenge
+# Each .py file in project subfolders has an individual challenge, where DocString will present the challenge
 # and the underneath code will show solution
 
